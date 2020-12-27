c2228ebd9cf624c6506365684a43f66de1134ee7
192091orito <74892550+192091orito@users.noreply.github.com>
2020-12-27 23:03:29 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-    '/https://192091orito.github.io/face2//',
+    '/https://192091orito.github.io/face2/',
 ];
 
 // インストール処理
